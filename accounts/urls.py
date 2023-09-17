50671d1fa18c53395c1655b58a221470ed164931
amirnjfpour <amirnjfpour37@yahoo.com>
2023-09-17 21:19:56 +0430

    add admin API with superuser permission added

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
-from accounts.views import RegisterView
+from accounts.views import RegisterView, AddAdminView
 
 urlpatterns = [
     path("register/", RegisterView.as_view(), name="register"),
     path("token/", TokenObtainPairView.as_view(), name="token"),
-    path("refresh-token/", TokenRefreshView.as_view(), name="refresh-token"),
+    path("refresh-token/", TokenRefreshView.as_view(), name="refresh_token"),
+    path("add-admin/", AddAdminView.as_view(), name="add_admin"),
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -1,7 +1,13 @@
 from rest_framework.generics import CreateAPIView
 
-from accounts.serializers import RegisterSerializer
+from accounts.permissions import IsSuperuser
+from accounts.serializers import RegisterSerializer, AddAdminSerializer
 
 
 class RegisterView(CreateAPIView):
     serializer_class = RegisterSerializer
+
+
+class AddAdminView(CreateAPIView):
+    serializer_class = AddAdminSerializer
+    permission_classes = (IsSuperuser,)
