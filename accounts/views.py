6ce0dd366286d5562e5b4b290b5c3406a76a1afd
amirnjfpour <amirnjfpour37@yahoo.com>
2023-09-17 21:09:29 +0430

    register API added

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,7 @@
-from django.shortcuts import render
+from rest_framework.generics import CreateAPIView
 
-# Create your views here.
+from accounts.serializers import RegisterSerializer
+
+
+class RegisterView(CreateAPIView):
+    serializer_class = RegisterSerializer
