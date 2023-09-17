ee0df88ee51f55d9bde8deac6d07be3a62e57636
amirnjfpour <amirnjfpour37@yahoo.com>
2023-09-17 21:12:20 +0430

    token and refresh token APIs added

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
-
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 from accounts.views import RegisterView
 
 urlpatterns = [
     path("register/", RegisterView.as_view(), name="register"),
+    path("token/", TokenObtainPairView.as_view(), name="token"),
+    path("refresh-token/", TokenRefreshView.as_view(), name="refresh-token"),
 ]
