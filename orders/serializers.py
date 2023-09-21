994494fc9aa8118ef99e3cf12c243848fdb27066
amirnjfpour <amirnjfpour37@yahoo.com>
2023-09-21 13:26:11 +0430

    get order with order items serializers added

--- orders/serializers.py
+++ orders/serializers.py
@@ -6,10 +6,26 @@ from orders.models import OrderItem, Order
 class BaseOrderSerializer(serializers.ModelSerializer):
     class Meta:
         model = Order
-        fields = ("user", )
+        fields = ("user",)
 
 
 class AddOrderItemSerializer(serializers.ModelSerializer):
     class Meta:
         model = OrderItem
         exclude = ("id", "count")
+
+
+class OrderItemSerializer(serializers.ModelSerializer):
+    item = serializers.CharField(source="item.name")
+
+    class Meta:
+        model = OrderItem
+        fields = ("item", "count")
+
+
+class OrderSerializer(serializers.ModelSerializer):
+    items = OrderItemSerializer(many=True)
+
+    class Meta:
+        model = Order
+        exclude = ("id", "user", "status")
